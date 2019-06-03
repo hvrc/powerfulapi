3106afad22923aaaaae601c163d271f8c9eac458
hvrc <harshavardhan@rajmachikar.com>
2019-06-03 17:25:25 +0530

    Added static database

--- greatapi/urls.py
+++ greatapi/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("static/", views.staticHome, name="staticHome"),
 ]
--- greatapi/views.py
+++ greatapi/views.py
@@ -4,10 +4,27 @@ from django.views.decorators.csrf import csrf_exempt
 from django.templatetags.static import static
 from urllib.parse import unquote
 from source.chimpanzee import *
+from static.database import *
 
 @csrf_exempt
 def home(request):
     videos = getVideos()
+    filteredVideos = getFilteredVideos(
+        videos=videos,
+        guest=unquote(request.GET.get("guest", "")),
+        category=unquote(request.GET.get("category", "All")),
+        sort=request.GET.get("sort", "Date"),
+        order=request.GET.get("order", "ascending")
+    )
+    return JsonResponse(
+        data=filteredVideos,
+        json_dumps_params={"indent": 4},
+        safe=False
+    )
+
+@csrf_exempt
+def staticHome(request):
+    videos = allVideos
     filteredVideos = getFilteredVideos(
         videos=videos,
         guest=unquote(request.GET.get("guest", "")),
